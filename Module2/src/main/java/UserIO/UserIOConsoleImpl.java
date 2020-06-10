f7275094abae5fda1e2b9041461825296cb4c70f
AhmedHussein-1 <a.m.hussein1994@hotmail.com>
2020-06-10 18:15:25 -0500

    I finished IO Impl

--- Module2/src/main/java/UserIO/UserIOConsoleImpl.java
+++ Module2/src/main/java/UserIO/UserIOConsoleImpl.java
@@ -6,60 +6,113 @@
 package UserIO;
 //
 //
+
+import java.util.Scanner;
+
+
 /**
  *
  * @author Maxka
  */
-public class UserIOConsoleImpl implements UserIO{
+public class UserIOConsoleImpl implements UserIO {
+    
+    Scanner sc = new Scanner(System.in);
 
     @Override
     public void print(String message) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println(message);
     }
 
     @Override
     public String readString(String prompt) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        print(prompt);
+        String userAnswer = sc.nextLine();
+        return userAnswer;    //sc.nextLine();
     }
 
     @Override
     public int readInt(String prompt) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        print(prompt);
+        String userAnswer = sc.nextLine();
+        return Integer.parseInt(userAnswer);
     }
+    
 
     @Override
     public int readInt(String prompt, int min, int max) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int value = -1;
+        do {
+            print(prompt);
+            String userAnswer = sc.nextLine();
+            value = Integer.parseInt(userAnswer);
+            if (value < min || value > max) {
+                print("Error value outside of range!");
+            }
+        } while (value < min || value > max);
+        return value;
     }
 
     @Override
     public double readDouble(String prompt) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        print(prompt);
+        String userAnswer = sc.nextLine();
+        return Double.parseDouble(userAnswer);
     }
 
     @Override
     public double readDouble(String prompt, double min, double max) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        double value = -1;
+        do {
+            print(prompt);
+            String userAnswer = sc.nextLine();
+            value = Double.parseDouble(userAnswer);
+            if (value < min || value > max) {
+                print("Error value outside of range!");
+            }
+        } while (value < min || value > max);
+        return value;
     }
 
     @Override
     public float readFloat(String prompt) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        print(prompt);
+        String userAnswer = sc.nextLine();
+        return Float.parseFloat(userAnswer);
     }
 
     @Override
     public float readFloat(String prompt, float min, float max) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        float value = -1;
+        do {
+            print(prompt);
+            String userAnswer = sc.nextLine();
+            value = Float.parseFloat(userAnswer);
+            if (value < min || value > max) {
+                print("Error value outside of range!");
+            }
+        } while (value < min || value > max);
+        return value;
     }
 
     @Override
     public long readLong(String prompt) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        print(prompt);
+        String userAnswer = sc.nextLine();
+        return Long.parseLong(userAnswer);
     }
 
     @Override
     public long readLong(String prompt, long min, long max) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        long value = -1;
+        do {
+            print(prompt);
+            String userAnswer = sc.nextLine();
+            value = Long.parseLong(userAnswer);
+            if (value < min || value > max) {
+                print("Error value outside of range!");
+            }
+        } while (value < min || value > max);
+        return value;
     }
     
 }
